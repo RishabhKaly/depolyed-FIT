9ed29d59c2f1290ecfae6a9ebb10224d3949f0b5
rishabh2k2 <74264023+rishabh2k2@users.noreply.github.com>
2025-03-14 19:11:10 -0700

    port 8000

--- app/app.py
+++ app/app.py
@@ -871,9 +871,6 @@ async def delete_clothing(item_id: int, request: Request):
 
 
 
-# if __name__ == "__main__":
-#    uvicorn.run(app="app.main:app", host="0.0.0.0", port=3306, reload=True)
-
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))  # Get Render's assigned port or default to 8000
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+   uvicorn.run(app="app.main:app", host="0.0.0.0", port=8000, reload=True)
+
